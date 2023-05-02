802ffd4f9c69557e71df375168720a62bb43f0a2
ꜱᴛᴀʀᴋ <120701871+NotStark@users.noreply.github.com>
2023-05-02 09:13:46 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -19,7 +19,7 @@ async def Sauce(bot_token,file_id):
     r = requests.post(f'https://api.telegram.org/bot{bot_token}/getFile?file_id={file_id}').json()
     file_path = r['result']['file_path']
     headers = {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'}
-    to_parse = f"https://images.google.com/searchbyimage??safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{bot_token}/{file_path}"
+    to_parse = f"https://images.google.com/searchbyimage?safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{bot_token}/{file_path}"
     r = requests.get(to_parse,headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
     result = {                            
