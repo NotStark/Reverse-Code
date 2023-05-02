ec71516513d05e613a5c58d540c86b7a85868d80
ꜱᴛᴀʀᴋ <120701871+NotStark@users.noreply.github.com>
2023-05-02 07:56:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pyrogram import Client,idle
 from pyrogram import filters
 import asyncio
 import requests
+from urllib.parse import quote_plus
+from bs4 import BeautifulSoup
 
 bot_token = "6141435415:AAE4HkjvE-BzwrfNMsNqYX-9mH87ca87qHg"
 
@@ -27,7 +29,22 @@ async def _pp(_,msg):
     file_id = replied.photo.file_id
     r = requests.post(f'https://api.telegram.org/bot{bot_token}/getFile?file_id={file_id}').json()
     file_path = r['result']['file_path']
-    print(f"https://images.google.com/searchbyimage??safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{bot_token}/{file_path}")
+    headers = {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'}
+    to_parse = f"https://images.google.com/searchbyimage??safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{bot_token}/{file_path}"
+    r = requests.get(to_parse,headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    result = {                            
+             "similar": '',
+             'output': ''
+         }
+    for similar_image in soup.find_all('input', {'class': 'gLFyf'}):
+         url = f"https://www.google.com/search?tbm=isch&q={quote_plus(similar_image.get('value'))}"
+         result['similar'] = url
+    for best in soup.find_all('div', {'class': 'r5a77d'}):
+        result['output'] = best.get_text(strip=True)
+    await msg.reply(results)
+
+ 
                       
     
 async def main():
