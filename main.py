ac6d00791685a67893d39651374ac1915a01cbe6
ꜱᴛᴀʀᴋ <120701871+NotStark@users.noreply.github.com>
2023-05-02 07:41:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def _pp(_,msg):
     if not replied.photo:
         return await msg.reply("reply to a photo pls")
     image_file_id = replied.photo.file_id
-    r = requests.post(f"https://api.telegram.org/file/bot6141435415:AAE4HkjvE-BzwrfNMsNqYX-9mH87ca87qHg/getFile/{image_file_id}")
+    r = requests.post(f"https://api.telegram.org/file/bot6141435415:AAE4HkjvE-BzwrfNMsNqYX-9mH87ca87qHg/getFile?file_id={image_file_id}")
     print(r.json())
                       
     
