d1eac9175759ae64d3ac2c75bce6f3f95274efb7
ꜱᴛᴀʀᴋ <120701871+NotStark@users.noreply.github.com>
2023-05-02 07:13:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from pyrogram import Client,idle
+from pyrogram import filters
+import asyncio
+
+
+app = Client(
+    "reverse_bot",
+    api_id = 11674810,
+    api_hash="9a64eb6bf7a4e8ba17dfa06efe6f2c6c",
+    bot_token="6141435415:AAE4HkjvE-BzwrfNMsNqYX-9mH87ca87qHg"
+)
+
+
+@app.on_message(filters.command("start"))
+async def _start(_,msg):
+    await msg.reply("hey there")
+
+async def main():
+    await app.start()
+    print("bot started")
+    await idle()
+    print("bot stopped")
+    
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
